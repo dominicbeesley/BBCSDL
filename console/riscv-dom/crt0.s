e1e6bfef9b014c3508c3dbe5eca1c5dac307604c
dominicbeesley <dominicbeesley@gmail.com>
2025-01-25 18:13:21 +0000

    FIX: flags is a byte - was causing address alignment exceptions

--- console/riscv-dom/crt0.s
+++ console/riscv-dom/crt0.s
@@ -21,6 +21,7 @@ bsssk:
 error_handler:
    addi    a1, a0, 4          # a1 = error smessage
    lw      a0, (a0)           # a0 = error code
+   andi    a0, a0, 0xFF       # DB: bodge to >0, <256
 # pass on to BBC Basic's error handler:
 #     void error(int, const char *);
 # this never returns
@@ -31,8 +32,8 @@ escape_handler:
    andi    a0, a0, 64         # mask escape flag
    slli    a0, a0, 1          # shift escape flag into bit 7
    la      t1, flags          # get the address of the flags variable
-   lw      t0, (t1)           # read the flags variable from memory
+   lb      t0, (t1)           # read the flags variable from memory
    andi    t0, t0, 0xffffff7f # clear bit 7
    or      t0, t0, a0         # copy escape flag to bit 7
-   sw      t0, (t1)           # write the flags variable back to memory
+   sb      t0, (t1)           # write the flags variable back to memory
    ret
